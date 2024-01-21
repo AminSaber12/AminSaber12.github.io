ea53c961dd155bd2f36f129fea03199a5ed57624
AminSaber12 <aminsaber0608@gmail.com>
2024-01-21 13:38:16 -0500

    Updates

---
+++ precache-manifest.60eea2284a17483c6b23da5bfcbbaca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc048e053aa2dd44f94",
+    "url": "/static/css/main.4651ad4d.chunk.css"
+  },
+  {
+    "revision": "5cc048e053aa2dd44f94",
+    "url": "/static/js/main.5cc048e0.chunk.js"
+  },
+  {
+    "revision": "f6dbdf81155b4f7fbe3c",
+    "url": "/static/js/1.f6dbdf81.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d6a4277f395e68502b5fcafdde06350a",
+    "url": "/index.html"
+  }
+];
